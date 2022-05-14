e6250dc84d50c9ed084d0f98cfdcb24c023ce2a2
MorozOO <Vx7nneN8508B>
2022-05-14 11:32:42 +0300

    lesson2 14.05 oop class start

---
+++ 2.py
@@ -0,0 +1,20 @@
+class Monitor:
+    displayResolution = 0
+    screenSize = 0
+    rate = 0
+    def __init__(self,px,dm,Hz):
+        print("Create nem Monitor")
+        self.displayResolution = px
+        self.screenSize = dm
+        self.rate = Hz
+    def printInfo(self,name):
+        print(f"{name:-^10}")
+        print("Display Resolution:", self.displayResolution)
+        print("Screen Size:", self.screenSize)
+        print("Image refresh rate:", self.rate)
+
+lg = Monitor(1500,500,1000)
+philips = Monitor(500,345,6789)
+lg.printInfo("LG")
+philips.printInfo("PHILIPS")
+
