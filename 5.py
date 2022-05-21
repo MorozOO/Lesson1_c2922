ab72f43224d5f970abd119f8f9ce6f9ae202cfa3
MorozOO <Vx7nneN8508B>
2022-05-21 10:44:37 +0300

    Inheritance of classes

---
+++ 5.py
@@ -0,0 +1,11 @@
+class Bird:
+    color = 0
+    def fly(self):
+        pass
+class Flamingo(Bird):
+    migration = True
+    def standOfOneLeg(self):
+        pass
+class Sparow(Flamingo):
+    def jumping(self):
+        pass
