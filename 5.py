7832a1eeea3be4efe5182c9e0f8afe804897a94f
MorozOO <Vx7nneN8508B>
2022-05-21 12:04:57 +0300

    Homeworke 6

--- 5.py
+++ 5.py
@@ -1,11 +1,11 @@
-class Bird:
-    color = 0
-    def fly(self):
-        pass
-class Flamingo(Bird):
-    migration = True
-    def standOfOneLeg(self):
-        pass
-class Sparow(Flamingo):
-    def jumping(self):
-        pass
+# class Bird:
+#     color = 0
+#     def fly(self):
+#         pass
+# class Flamingo(Bird):
+#     migration = True
+#     def standOfOneLeg(self):
+#         pass
+# class Sparow(Flamingo):
+#     def jumping(self):
+#         pass
